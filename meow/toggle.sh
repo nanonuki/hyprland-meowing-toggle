d9239380a888e7d1f195e968b55a0bf75aaceb4a
nano <caique.moura1406@gmail.com>
2025-06-29 18:58:44 -0300

    Update toggle.sh

--- meow/toggle.sh
+++ meow/toggle.sh
@@ -6,15 +6,15 @@ CONFIG_FILE="$HOME/.config/hypr/hyprland.conf"
 TEMP_FILE="/tmp/hyprland_temp.conf"
 TOGGLE_FILE="/tmp/hypr_keybind_toggle"
 
-# Check if the toggle file exists
+# check if toggle file exists
 if [ -f "$TOGGLE_FILE" ]; then
-    # Disable the target keybind
+    # disable target keybind
     sed '/bind = , space, exec, ~\/.config\/hypr\/scripts\/meow\/randomizer.sh/d' "$CONFIG_FILE" > "$TEMP_FILE"
     mv "$TEMP_FILE" "$CONFIG_FILE"
     rm "$TOGGLE_FILE"
     notify-send "Keybind" "MEOW - Disabled"
 else
-    # Enable the target keybind
+    # enable the target keybind
     echo "bind = , space, exec, ~/.config/hypr/scripts/meow/randomizer.sh" >> "$CONFIG_FILE"
     touch "$TOGGLE_FILE"
     notify-send "Keybind" "WOOF - Enabled"
